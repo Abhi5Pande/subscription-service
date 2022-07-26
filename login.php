96bd13f667c13dbb3c3f564a1f7bbc69e081c3e8
abhijit <abhi.pachpande@gmail.com>
2022-07-26 16:40:41 +0530

    login working with username with updated UI

--- login.php
+++ login.php
@@ -14,22 +14,21 @@ if (! empty($_POST["login-btn"])) {
 	rel="stylesheet" />
 <link href="assets/css/user-registration.css" type="text/css"
 	rel="stylesheet" />
-<script src="vendor/jquery/jquery-3.3.1.js" type="text/javascript"></script>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
 </HEAD>
 <BODY>
-	<div class="phppot-container">
-		<div class="sign-up-container">
-			<div class="login-signup">
-				<a href="user-registration.php">Sign up</a>
-			</div>
-			<div class="signup-align">
+<div class="container-fluid login-page">
+        <div class="login-form-div">
+            <h5 class="text-center heading">Login</h5>
+            <div class="signup-align">
 				<form name="login" action="" method="post"
-					onsubmit="return loginValidation()">
-					<div class="signup-heading">Login</div>
+					onsubmit="return loginValidation()" class="form-group login-form">
 				<?php if(!empty($loginResult)){?>
 				<div class="error-msg"><?php echo $loginResult;?></div>
 				<?php }?>
-				<div class="row">
+				<!-- <div class="row">
 						<div class="inline-block">
 							<div class="form-label">
 								Username<span class="required error" id="username-info"></span>
@@ -46,12 +45,25 @@ if (! empty($_POST["login-btn"])) {
 							<input class="input-box-330" type="password"
 								name="login-password" id="login-password">
 						</div>
-					</div>
+					</div> -->
+					<label for="email">Email</label>
+                    <input type="text" name="username" class="form-control" placeholder="Enter Email id" id="username"
+                        validate>
+					<!-- Password -->
+					<label for="password">Password</label>
+                    <input type="password" class="form-control" name="login-password" id="login-password" placeholder="Password" required>
+                    <div class="form-check">
+                        <input class="form-check-input" type="checkbox" value="" id="rememberme">
+                        <label class="form-check-label" for="flexCheckDefault">
+                            Remember me
+                        </label>
+                    </div>
 					<div class="row">
 						<input class="btn" type="submit" name="login-btn"
 							id="login-btn" value="Login">
 					</div>
 				</form>
+				<p class="text-center">New to MyApp? <a  class="text-decoration-none" href="register.php">Sign up</a></p>
 			</div>
 		</div>
 	</div>
