273f14fdc04acc0932c5a33e1787dfadd3d1fff2
abhijit <abhi.pachpande@gmail.com>
2022-07-26 22:14:38 +0530

    basic front-End with functionaliy implemented

--- home.php
+++ home.php
@@ -1,4 +1,5 @@
 <?php
+require './lib.php';
 session_start();
 if (isset($_SESSION["username"])) {
     $username = $_SESSION["username"];
@@ -33,7 +34,11 @@ if (isset($_SESSION["username"])) {
 			<span class="login-signup"><a href="logout.php">Logout</a></span>
 </div> -->
 <div class="container-fluid">
+    <div class="home-heading">
+        <h4 class="text-center">Choose the right plan for you</h4>
+    </div>
     <div class="subscription-body">
+        <div class="sub-heading">
         <div class="row">
             <div class="col-4">
                 <div class="btn-group monthyear" role="group" >
@@ -41,7 +46,163 @@ if (isset($_SESSION["username"])) {
                     <button class="btn Year-btn" id="yearly">Yearly</button>
                 </div>
             </div>
+            <div class="col-8">
+                <div class="row">
+                    <div class="col-3 mobile">
+                        mobile
+                    </div>
+                    <div class="col-3 basic">
+                        Basic
+                    </div>
+                    <div class="col-3 standard">
+                        Standard
+                    </div>
+                    <div class="col-3 premium">
+                        Premium
+                    </div>
+                </div>
+            </div>
+            
         </div>
+        </div>
+        <?php 
+        
+        $sql1 = "SELECT * FROM `plans` WHERE plan_id = 1;";
+        $sql2 = "SELECT * FROM `plans` WHERE plan_id = 2;";
+        $sql3 = "SELECT * FROM `plans` WHERE plan_id = 3;";
+        $sql4 = "SELECT * FROM `plans` WHERE plan_id = 4;";
+        $result1 = mysqli_query($conn,$sql1);
+        $result2 = mysqli_query($conn,$sql2);
+        $result3 = mysqli_query($conn,$sql3);
+        $result4 = mysqli_query($conn,$sql4);
+        $row1 = mysqli_fetch_array($result1);
+        $row2 = mysqli_fetch_array($result2);
+        $row3 = mysqli_fetch_array($result3);
+        $row4 = mysqli_fetch_array($result4);
+        
+                echo '<div class="row" id="price_m">
+                <div class="col-4">
+                    Monthly price
+                </div>
+                <div class="col-8">
+                    <div class="row">
+                        <div class="col-3 mobile">
+                            $'.$row1[2].'
+                        </div>
+                        <div class="col-3 basic">
+                        $'.$row2[2].'
+                        </div>
+                        <div class="col-3 standard">
+                        $'.$row3[2].'
+                        </div>
+                        <div class="col-3 premium">
+                        $'.$row4[2].'
+                        </div>
+                    </div>
+                </div>
+                <hr>
+            </div>
+            
+            <div class="row d-none" id="price_y">
+                <div class="col-4">
+                   Yearly Price
+                </div>
+                <div class="col-8">
+                    <div class="row">
+                        <div class="col-3 mobile">
+                        $'.$row1[3].'
+                        </div>
+                        <div class="col-3 basic">
+                            $'.$row2[3].'
+                        </div>
+                        <div class="col-3 standard">
+                        $'.$row3[3].'
+                        </div>
+                        <div class="col-3 premium">
+                        $'.$row4[3].'
+                        </div>
+                    </div>
+                </div>
+                <hr>
+            </div>
+            
+            <div class="row">
+                <div class="col-4">
+                Video Quality
+                </div>
+                <div class="col-8">
+                    <div class="row">
+                        <div class="col-3 mobile">
+                        '.$row1[4].'
+                        </div>
+                        <div class="col-3 basic">
+                        '.$row2[4].'
+                        </div>
+                        <div class="col-3 standard">
+                        '.$row3[4].'
+                        </div>
+                        <div class="col-3 premium">
+                        '.$row4[4].'
+                        </div>
+                    </div>
+                </div>
+                <hr>
+            </div>
+            
+            <div class="row">
+                <div class="col-4">
+                  Resolution
+                </div>
+                <div class="col-8">
+                    <div class="row">
+                        <div class="col-3 mobile">
+                        '.$row1[5].'
+                        </div>
+                        <div class="col-3 basic">
+                        '.$row2[5].'
+                        </div>
+                        <div class="col-3 standard">
+                        '.$row3[5].'
+                        </div>
+                        <div class="col-3 premium">
+                        '.$row4[5].'
+                        </div>
+                    </div>
+                </div>
+                <hr>
+            </div>
+            
+            <div class="row">
+            <div class="col-4">
+              Devices you can use to watch
+            </div>
+            <div class="col-8">
+                <div class="row">
+                    <div class="col-3 mobile">
+                        <p> Phone </p>
+                    </div>
+                    <div class="col-3 basic">
+                    <p> Phone </p>
+                    <p> Tablet </p>
+                    </div>
+                    <div class="col-3 standard">
+                    <p> Phone </p>
+                    <p> Tablet </p>
+                    <p>Computer</p>
+                    </div>
+                    <div class="col-3 premium">
+                    <p> Phone </p>
+                    <p> Tablet </p>
+                    <p>TV</p>
+                    </div>
+                </div>
+            </div>
+            <hr>
+        </div>';
+
+          
+        
+         ?>
     </div>
 </div>
 
@@ -49,18 +210,24 @@ if (isset($_SESSION["username"])) {
     var monthly = document.getElementById("monthly");
     console.log(monthly);
     var yearly = document.getElementById("yearly");
-    console.log(yearly)
+    console.log(yearly);
+    var price_m = document.getElementById("price_m");
+    var price_y = document.getElementById("price_y");
     monthly.addEventListener("click",function(){
         console.log("month button pressed");
         let current = document.getElementsByClassName("btn-active");
         current[0].className = current[0].className.replace(" btn-active" , "")
         monthly.className += " btn-active"
+        price_y.className += " d-none"
+        price_m.classList.remove("d-none")
     });
     yearly.addEventListener("click",function(){
         console.log("year button pressed")
         let current = document.getElementsByClassName("btn-active");
         current[0].className = current[0].className.replace(" btn-active" , "")
         yearly.className += " btn-active"
+        price_m.className += " d-none"
+        price_y.classList.remove("d-none")
     });
 </script>
 
